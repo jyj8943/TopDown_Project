85eb3d03dfe97af5dfe4145fe04dc351327600fa
jyj8943 <jyj8943@gmail.com>
2025-07-23 15:56:59 +0900

    3-5 스탯 시스템 기초 구성

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -26,11 +26,13 @@ public class BaseController : MonoBehaviour
     private float knockBackDuration = 0.0f;
 
     protected AnimationHandler animationHandler;
+    protected StatHandler statHandler;
 
     protected virtual void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         animationHandler = GetComponent<AnimationHandler>();
+        statHandler = GetComponent<StatHandler>();
     }
 
     protected virtual void Start()
@@ -60,7 +62,7 @@ public class BaseController : MonoBehaviour
 
     private void Movement(Vector2 direction)
     {
-        direction = direction * 5;
+        direction = direction * statHandler.Speed;
         if (knockBackDuration > 0.0f)
         {
             direction *= 0.2f;

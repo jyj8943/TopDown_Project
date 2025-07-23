6b6392d7069c25a3fcc28562ccd28284e5f72887
jyj8943 <jyj8943@gmail.com>
2025-07-23 20:21:23 +0900

    3-9 적 오브젝트 기초 구성

---
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyManager : MonoBehaviour
+{
+    private Coroutine waveRoutine;
+
+    [SerializeField] private List<GameObject> enemyPrefabs;
+
+    [SerializeField] private List<Rect> spawnAreas;
+    [SerializeField] private Color gizmoColor = new Color(1, 0, 0, 0.3f);
+    private List<EnemyController> activeEnemies = new List<EnemyController>();
+
+    private bool enemySpawnComplite;
+
+    [SerializeField] private float timeBetweenSpawns = 0.2f;
+    [SerializeField] private float timeBetweenWaves = 1f;
+
+    public void StartWave(int waveCount)
+    {
+        if (waveRoutine != null)
+            StopCoroutine(waveRoutine);
+        waveRoutine = StartCoroutine(SpawnWave(waveCount));
+    }
+
+    public void StopWave()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator SpawnWave(int waveCount)
+    {
+        enemySpawnComplite = false;
+        yield return new WaitForSeconds(timeBetweenWaves);
+
+        for (int i = 0; i < waveCount; i++)
+        {
+            yield return new WaitForSeconds(timeBetweenSpawns);
+            SpawnRandomEnemy();
+        }
+
+        enemySpawnComplite = true;
+    }
+
+    private void SpawnRandomEnemy()
+    {
+        if (enemyPrefabs.Count == 0 || spawnAreas.Count == 0)
+        {
+            Debug.LogWarning("Enemy Prefabs 또는 Spawn Areas가 설정되지 않았습니다.");
+            return;
+        }
+
+        GameObject randomPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Count)];
+
+        Rect randomArea = spawnAreas[Random.Range(0, spawnAreas.Count)];
+
+        Vector2 randomPos = new Vector2(
+            Random.Range(randomArea.xMin, randomArea.xMax),
+            Random.Range(randomArea.yMin, randomArea.yMax));
+
+        GameObject spawnEnemy = Instantiate(randomPrefab, new Vector3(randomPos.x, randomPos.y), quaternion.identity);
+        EnemyController enemyController = spawnEnemy.GetComponent<EnemyController>();
+        
+        activeEnemies.Add(enemyController);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (spawnAreas == null) return;
+
+        Gizmos.color = gizmoColor;
+        foreach (var area in spawnAreas)
+        {
+            Vector3 center = new Vector3(area.x + area.width / 2, area.y + area.height / 2);
+            Vector3 size = new Vector3(area.width, area.height);
+            
+            Gizmos.DrawCube(center, size);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartWave(1);
+        }
+    }
+}
